a01703837d8fa9a249d0c7731f47a066de940e86
yeexuean <yeexuean0502@gmail.com>
2025-03-14 23:10:21 +0000

    update to mvc format 1

--- app/controllers/auth.php
+++ app/controllers/auth.php
@@ -49,7 +49,6 @@ function isAdminBySessionId() {
   }
 }
 
-
 // Logout
 function logout() {
     session_destroy();
---
+++ app/views/layouts/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Bookshop</title>
+    
+    <link rel="stylesheet" href="app/public/assets/css/style.css">
+</head>
+<body>
+
+  <header class="header">
+      <div class="header-1">
+        <div class="section1">
+          <ul class="nav-menus">
+            <li>
+              <a href="">Home</a>
+              <img src="app/public/assets/icons/dropdown.svg" alt="dropdown icon" />
+            </li>
+            <li>
+              <a href="">Categories</a>
+              <img src="app/public/assets/icons/dropdown.svg" alt="dropdown icon" />
+            </li>
+            <li>
+              <a href="">Authors</a>
+              <img src="app/public/assets/icons/dropdown.svg" alt="dropdown icon" />
+            </li>
+            <li><a href="">Contact us</a></li>
+          </ul>
+        </div>
+
+        <div class="section2">
+          <a href="index.html"
+            ><img src="logo.svg" alt="store logo" class="logo" />
+          </a>
+        </div>
+
+        <div class="section3">
+          <div class="nav-icons">
+            <a href=""> <img src="icon/search.svg" alt="search bar" /></a>
+            <a href="shopping-cart.html">
+              <img src="icon/cart.svg" alt="shopping cart"
+            /></a>
+            <a href="sign-in.html">
+              <img src="icon/profile.svg" alt="user-profile"
+            /></a>
+          </div>
+        </div>
+      </div>
+    </header>
+  </body>
+</head>
