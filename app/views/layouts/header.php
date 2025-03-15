92483a847a1df7696312b572e349d94fb917fe11
yeexuean <yeexuean0502@gmail.com>
2025-03-15 01:15:37 +0000

    html to php
    converting all the html to php first

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Bookshop</title>
     
-    <link rel="stylesheet" href="app/public/assets/css/style.css">
+    <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
 
